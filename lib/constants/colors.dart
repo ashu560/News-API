b5ce3c57b18dfd9812dd2901f6637af1709332df
Ashutosh <ashu.deshmukh560@gmail.com>
2024-05-30 22:12:31 +0530

    first commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+// Studios palette
+class StudioColors {
+  static const Color lightBeige = Color(0xFFCCD5AE);
+  static const Color lightCream = Color(0xFFE9EDC9);
+  static const Color lightIvory = Color(0xFFFEFAE0);
+  static const Color lightPeach = Color(0xFFFAEDCD);
+  static const Color warmBrown = Color(0xFFD4A373);
+  static const Color white = Color.fromARGB(255, 244, 245, 244);
+}
+
+// Night mode palette
+class NightModeColors {
+  static const Color darkGray = Color(0xFF222831);
+  static const Color charcoal = Color(0xFF393E46);
+  static const Color aquaBlue = Color(0xFF00ADB5);
+  static const Color lightGray = Color(0xFFEEEEEE);
+  static const Color white = Color.fromARGB(255, 244, 245, 244);
+}
+
+// Fresh color palette
+class FreshColors {
+  static const Color lightPink = Color(0xFFCDB4DB);
+  static const Color lightRose = Color(0xFFFFC8DD);
+  static const Color softPink = Color(0xFFFFAFCC);
+  static const Color babyBlue = Color(0xFFBDE0FE);
+  static const Color skyBlue = Color(0xFFA2D2FF);
+  static const Color white = Color.fromARGB(255, 244, 245, 244);
+}
+
+// Sea theme palette
+class SeaColors {
+  // Changed name from FourthColors
+  static const Color darkBlue = Color(0xFF265073);
+  static const Color tealBlue = Color(0xFF2D9596);
+  static const Color lightGray = Color(0xFFF1FADA);
+  static const Color white = Color.fromARGB(255, 244, 245, 244);
+}
+
+// Dark theme palette
+class DarkThemeColors {
+  // Changed name from FifthColors
+  static const Color darkNavy = Color(0xFF35374B);
+  static const Color darkBlue = Color(0xFF344955);
+  static const Color slateBlue = Color(0xFF50727B);
+  static const Color forestGreen = Color(0xFF78A083);
+  static const Color white = Color.fromARGB(255, 244, 245, 244);
+}
---
+++ lib/screens/settings.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Settings extends StatefulWidget {
+  const Settings({super.key});
+
+  @override
+  State<Settings> createState() => SsettingsState();
+}
+
+class SsettingsState extends State<Settings> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Settings Page...!'),
+    );
+  }
+}
