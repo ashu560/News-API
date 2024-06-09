8c0ac4b27c6ac5bae53eb11a8588a136523e8a63
Ashutosh <ashu.deshmukh560@gmail.com>
2024-06-09 13:45:44 +0530

    fixed storing API key in safe place.

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,10 @@ import 'package:boilerplate/Navigation%20Bar/navbar.dart';
 import 'package:boilerplate/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
+  await dotenv.load(fileName: "assets/.env");
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
